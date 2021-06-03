a3de8c571fae8b6d6b2546aeac122d9111aca9ef
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2021-06-03 18:09:07 +0100

    update inspecciones notebook

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 docker build -t criskrus/jupyter -f .docker/jupyter/Dockerfile .
 
-docker run --rm --user root -p 8888:8888 -v $(pwd):/home/cristian.suarez/notebooks -e NB_UID=$(id -u) -e NB_GID=$(id -g) criskrus/jupyter
+docker run --rm --user root --env-file .env -p 8888:8888 -v $(pwd):/home/cristian.suarez/notebooks -e NB_UID=$(id -u) -e NB_GID=$(id -g) criskrus/jupyter
